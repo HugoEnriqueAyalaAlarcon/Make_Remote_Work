fc94af2ad570cc65b18460625f8959d997959f95
HugoEnriqueAyalaAlarcon <heaa84@hotmail.com>
2022-10-31 13:59:27 -0600

    poyecto_final

--- main.js
+++ main.js
@@ -1,48 +1,75 @@
 let Text_Completo_getyour ="Get your team in sync, no matter your location. Streamline processes, create team rituals, and watch productivity soar, without sacrificing efficiency. have your entire work team under control, but without limiting their creativity since they can work from home";
 let Text_less_getyour="Get your team in sync, no matter your location. Streamline processes, create team rituals, and watch productivity soar. ";
-console.log(document.getElementById("contener__text-parafo-dinamico"));
+let button_Status=0;
+let registro_Status=0;
 
-function button_learn_less(){
-    /*alert("botton")*/
-    document.getElementById("contener__text-parafo--dinamico").innerHTML=Text_less_getyour;
-    document.getElementById("contener__text-button--dinamico").innerHTML="learn more";
-    document.getElementById("contener__button-onclik").onclick = function(){button_learn_more();}
-}
+/*******************************variables ****************************/
+let user="",cUser="",
+pass="",cPass="",
+card,
+button__Login,
+button__submint,
+button__cancel,
+button__Register;
 
-function button_learn_more(){
-    document.getElementById("contener__text-parafo--dinamico").innerHTML=Text_Completo_getyour;
-    document.getElementById("contener__text-button--dinamico").innerHTML="learn less";
-    document.getElementById("contener__button-onclik").onclick = function(){button_learn_less();}
-}
-
-document.getElementById("contener__button-onclik").onclick = function(){button_learn_more();}
-
-
-/*********boton login */
-let user="Funval",cUser,
-    pass="Funval",cPass,
-    button,button__Login,card;
-button=document.getElementById("button__submint");
-button__Login=document.getElementById("button__Login");
+/**********************************llamado de elementos ***************/
 card=document.getElementById("card");
-console.log(button);
+button__Login=document.getElementById("button__Login");
+button__submint=document.getElementById("button__submint");
+button__cancel=document.getElementById("button__cancel");
+button__Register=document.getElementById("nav__Botton-Register");
+button__Learn=document.getElementById("contener__button-onclik"); /*cambiar id*/
 
+/**************************************funciones*******************************/
 let getData=function(){
-    cUser=document.getElementById("card__user").value;
-    cPass=document.getElementById("card__pass").value;
-    console.log(cUser);
-    console.log(cPass);
-    if(cUser=="Funval" && cPass=="Funval"){
-        alert("Bienvenido")
+    if (registro_Status==1){
+        user=document.getElementById("card__user").value;
+        pass=document.getElementById("card__pass").value;
+        alert("usuario registrado")
         card.style.display="none";
-        button__Login.innerHTML="Funval";
-        button__Login.style.color="black";
     }
+    if (registro_Status==0){
+        cUser=document.getElementById("card__user").value;
+        cPass=document.getElementById("card__pass").value;
+        if(cUser==user && cPass==pass){
+            alert("Bienvenido "+user);
+            card.style.display="none";
+            button__Login.innerHTML=user;
+            button__Login.style.color="black";
+        }
+    }
+}
+let register=function(){
+    card.style.display="flex";
+    registro_Status=1;
+
+}
+let noCardVisible=function(){
+    card.style.display="none";
 }
 let cardVisible=function(){
     card.style.display="flex";
+    registro_Status=0;
+}
+let muestraMas=function(){ //***variable muestraMas como funcion */
+    console.log(button_Status);
+    if(button_Status==0){
+        document.getElementById("contener__text-parafo--dinamico").innerHTML=Text_Completo_getyour;
+        document.getElementById("contener__text-button--dinamico").innerHTML="learn less";
+        button_Status=1;
+        console.log(button_Status);
+    }else{
+        document.getElementById("contener__text-parafo--dinamico").innerHTML=Text_less_getyour;
+        document.getElementById("contener__text-button--dinamico").innerHTML="learn more";
+        button_Status=0;
+    }
 }
 
-button.onclick=getData;
+
+//////////*****************llamar funciones con clik ************************/
 button__Login.onclick=cardVisible;
-console.log(cUser);
+button__submint.onclick=getData;
+button__cancel.onclick=noCardVisible;
+button__Learn.onclick=muestraMas;
+button__Register.onclick=register;
+
